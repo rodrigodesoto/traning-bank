94083ae8f52dd559064506243fc82fb1d7ec0fd9
Rodrigo <rodrigo.soto@ext.sinqia.com.br>
2020-06-15 18:07:58 -0300

    Parte 2

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Typed from 'typed.js';
 
 @Component({
   selector: 'app-content',
@@ -10,6 +11,20 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const options = {
+      stringsElement: '#typed-strings',
+      strings: ['Training banking juros amigo', 'Peça a portabilidade e venha ser Training Bankinhg hoje mesmo', 'Inovavor digital e seguro'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 200,
+      smartBackspace: true,
+      fadeOut: true,
+      showCursor: false,
+      startDelay: 1000,
+      loop: true
+    };
+
+    const typed = new Typed('.typing-element', options);
   }
 
 }
